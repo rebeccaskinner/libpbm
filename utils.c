b0325c1fd1c9a4af1ded813f122d9390290c5725
Rebecca Skinner <rebecca@rebeccaskinner.net>
2011-05-11 22:56:11 -0500

    parsing type/width/height and skipping comments, error parsing test data

--- utils.c
+++ utils.c
@@ -16,7 +16,7 @@ void* print_err(void* rval, const char* fmt, ...)
 {
     va_list ap;
     va_start(ap, fmt);
-    vprintf(dest,fmt,ap);
+    vfprintf(stderr,fmt,ap);
     va_end(ap);
     return rval;
 }
