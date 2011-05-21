a75bb94d93db054ca04a1e7e5e7a0590f5829c70
Tim Skinner <tim@timskinner.net>
2011-05-20 19:25:42 -0500

    added files

---
+++ utils.h
@@ -0,0 +1,5 @@
+#ifndef __PNM_UTILS_H__
+#define __PNM_UTILS_H__
+void* fprint_err(FILE* dest, void* rval, const char* fmt, ...) __attribute__((format(printf,3,4)));
+void* print_err(void* rval, const char* fmt, ...) __attribute__((format(printf,2,3)));
+#endif
